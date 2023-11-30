0120fc94d86f1df9c106a33429d892808c8d0e6d
4anonymoussubmission <150982338+4anonymoussubmission@users.noreply.github.com>
2023-11-30 18:24:22 +0800

    Add files via upload

---
+++ scripts/run_r18.sh
@@ -0,0 +1,1 @@
+ python main.py --config configs/resnet18.yaml --multigpu 0 --checkpoint /path/to/dense/resnet18.pth --prune_rate 0.9        
---
+++ scripts/run_r50.sh
@@ -0,0 +1,1 @@
+ python main.py --config configs/resnet50.yaml --multigpu 0 --checkpoint /path/to/dense/resnet50.pth --prune_rate 0.9 
